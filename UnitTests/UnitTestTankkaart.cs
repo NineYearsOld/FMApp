4d9855528be8afabeeb65e7e0880ba6b77ea288e
molo650 <langiejeroen9050@gmail.com>
2021-11-30 19:55:15 +0100

    Unit tests

--- UnitTests/UnitTestTankkaart.cs
+++ UnitTests/UnitTestTankkaart.cs
@@ -1,11 +1,43 @@
-﻿using System;
+﻿using BusinessLayer.Entities;
+using BusinessLayer.Exceptions;
+using System;
 using Xunit;
 
 namespace UnitTests {
     public class UnitTestTankkaart {
         [Fact]
-        public void Test1() {
+        public void TestConstructor() {
+            int kaartnummer = 123456;
+            DateTime geldigheidsdatum = new DateTime(2022, 1, 1);
+            string pin = "1234";
+            string brandstof = "diesel";
+            int bestuurderid = 1;
 
+            Tankkaart t = new Tankkaart(kaartnummer, geldigheidsdatum, pin, brandstof, bestuurderid);
+            Assert.Equal(123456, t.KaartNummer);
+            Assert.Equal(new DateTime(2022, 1, 1), t.GeldigheidsDatum);
+            Assert.Equal("1234", t.Pincode);
+            Assert.Equal("diesel", t.Brandstoffen);
+            Assert.Equal(1, t.BestuurderId);
+
+            Assert.Throws<TankkaartException>(() => new Tankkaart(kaartnummer, new DateTime(2021, 1, 1), pin, brandstof, bestuurderid)); // geldigheidsdatum is al verlopen
+            Assert.Throws<TankkaartException>(() => new Tankkaart(kaartnummer, geldigheidsdatum, pin, brandstof, 0)); // bestuurderid = 0
+            Assert.Throws<TankkaartException>(() => new Tankkaart(kaartnummer, geldigheidsdatum, pin, brandstof, -3)); // bestuurderid is negatief
+            Assert.Throws<TankkaartException>(() => new Tankkaart(kaartnummer, geldigheidsdatum, "", brandstof, bestuurderid)); // Lege pin
+            Assert.Throws<TankkaartException>(() => new Tankkaart(kaartnummer, geldigheidsdatum, "123", brandstof, bestuurderid)); // pin te kort
+            Assert.Throws<TankkaartException>(() => new Tankkaart(kaartnummer, geldigheidsdatum, "12345", brandstof, bestuurderid)); // pin te lang
+            Assert.Throws<TankkaartException>(() => new Tankkaart(kaartnummer, geldigheidsdatum, null, brandstof, bestuurderid)); // pin = null
+        }
+
+        [Fact]
+        public void TestUpdateGeldigheidsdatum() {
+            int kaartnummer = 123456;
+            DateTime geldigheidsdatum = new DateTime(2022, 1, 1);
+            string pin = "1234";
+            string brandstof = "diesel";
+            int bestuurderid = 1;
+
+            Tankkaart t = new Tankkaart(kaartnummer,geldigheidsdatum,pin,brandstof,bestuurderid);
         }
     }
 }
